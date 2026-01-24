8b953c874beb4c40c3c0b6d922e445fbf23760b2
Alexander Hardinan <blackchef.alex@gmail.com>
2026-01-24 11:56:17 +0700

    Force-load Buy Me a Coffee widget via DOM injection

--- components/BuyMeCoffee.tsx
+++ components/BuyMeCoffee.tsx
@@ -1,20 +1,30 @@
-'use client'
+"use client"
 
-import Script from 'next/script'
+import { useEffect } from "react"
 
 export default function BuyMeCoffee() {
-  return (
-    <Script
-      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-      strategy="afterInteractive"
-      data-name="BMC-Widget"
-      data-id="chefalex"
-      data-description="Support me on Buy me a coffee!"
-      data-message=""
-      data-color="#5F7FFF"
-      data-position="Right"
-      data-x_margin="18"
-      data-y_margin="18"
-    />
-  )
+  useEffect(() => {
+    // prevent double-injection
+    if (document.querySelector('[data-name="BMC-Widget"]')) return
+
+    const script = document.createElement("script")
+    script.src = "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+    script.async = true
+    script.setAttribute("data-name", "BMC-Widget")
+    script.setAttribute("data-id", "chefalex")
+    script.setAttribute("data-description", "Support me on Buy me a coffee!")
+    script.setAttribute("data-message", "")
+    script.setAttribute("data-color", "#5F7FFF")
+    script.setAttribute("data-position", "Right")
+    script.setAttribute("data-x_margin", "18")
+    script.setAttribute("data-y_margin", "18")
+
+    document.body.appendChild(script)
+
+    return () => {
+      script.remove()
+    }
+  }, [])
+
+  return null
 }
